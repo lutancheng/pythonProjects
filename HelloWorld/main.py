19e026d16d2910e7873ad797eef057f6a4bae087
lutancheng <1160936938@qq.com>
2019-10-02 16:41:56 +0800

    3dSpin

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 # filename: main.py
 import web
-
+import os
+root = os.path.dirname(__file__) 
+render = web.template.render(os.path.join(root, '3dSpin/'), cache=False)
 urls = (
     '/wx', 'Handle',
 )
 
+#render=web.template.render('3dSpin', cache=False)
+
 class Handle(object):
     def GET(self):
-        return "hello, this is handle view"
+        #return "hello, this is handle view"
+        return render.index()
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
